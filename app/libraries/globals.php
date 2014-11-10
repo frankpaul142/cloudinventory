7508d7f1be213e9ca29edb6dfe6851c387f1b45a
Xavier Rivas <escuok@gmail.com>
2014-11-09 23:32:31 -0500

    alerts fix

--- app/libraries/globals.php
+++ app/libraries/globals.php
@@ -1,135 +1,75 @@
-<?php 
-
- 
+<?php
 
 class Globals
-
 {
-
     public static function triggerAlerts($alertsId, $additional) {
-
         $message = '';
 
-
-
         switch ($alertsId) {
-
             case 1:
-
                 if (isset($additional['productId'])) {
-
                     $product = Product::find($additional['productId']);
-
                     if ( ! is_null($product)) {
-
                         if ($product->stock > 0 AND $product->stock < $product->minimum_stock) {
-
                             $message = 'El producto ' . $product->name . ' ha bajado del stock mínimo.';
-
                         }
-
                     }
-
                 }
-
                 break;
 
             case 2:
-
                 if (isset($additional['productId'])) {
-
                     $product = Product::find($additional['productId']);
-
                     if ( ! is_null($product)) {
-
                         if ($product->stock == 0) {
-
                             $message = 'El producto ' . $product->name . ' no tiene stock.';
-
                         }
-
                     }
-
                 }
-
                 break;
 
             case 3:
-
                 if (isset($additional['userId'])) {
-
                     $user = User::find($additional['userId']);
-
                     if ( ! is_null($user)) {
-
                         $message = 'El usuario ' . $user->display_name . ' necesita aprobación.';
-
                     }
-
                 }
-
                 break;
 
             case 4:
-
                 if (isset($additional['productId'])) {
-
-                    $product = User::find($additional['productId']);
-
+                    $product = Product::find($additional['productId']);
                     if ( ! is_null($product)) {
-
                         $message = 'El producto ' . $product->name . ' ha sido creado.';
-
                     }
-
                 }
-
                 break;
 
             case 5:
-
                 if (isset($additional['supplierOrderId'])) {
-
-                    $supplierOrder = User::find($additional['supplierOrderId']);
-
+                    $supplierOrder = SupplierOrder::find($additional['supplierOrderId']);
                     if ( ! is_null($supplierOrder)) {
-
                         $message = 'Se ha creado un nuevo pedido al proveedor ' . $supplierOrder->supplier->name . '.';
-
                     }
-
                 }
-
                 break;
 
             case 6:
-
                 if (isset($additional['supplierOrderId'])) {
-
-                    $supplierOrder = User::find($additional['supplierOrderId']);
-
+                    $supplierOrder = SupplierOrder::find($additional['supplierOrderId']);
                     if ( ! is_null($supplierOrder)) {
-
                         $message = 'Se ha recibido un pedido del proveedor ' . $supplierOrder->supplier->name . '.';
-
                     }
-
                 }
-
                 break;
 
             default:
-
                 break;
-
         }
 
-
-
         if ($message != '') {
-
             static::__sendAlert($alertsId, $message);
-
         }
 
     }
@@ -137,43 +77,26 @@ class Globals
 
 
     private static function __sendAlert($alertsId, $message) {
-
         $alert = Alert::find($alertsId);
-
         if ( ! is_null($alert)) {
-
             $alerts_to = $alert->users;
-
             foreach ($alerts_to as $current) {
-
                 if ($current->pivot->to_facebook) {
-
                     static::__sendFacebook($current->facebook_id, $message);
-
                 }
-
                 if ($current->pivot->to_sms) {
-
                     static::__sendSms($current->mobile, $message);
-
                 }
-
                 if ($current->pivot->to_email) {
-
                     static::__sendEmail(array($current->email, $current->display_name), $message);
-
                 }
-
             }
-
         }
-
     }
 
 
 
     private static function __sendFacebook($to, $message) {
-
         $session = Facebook::process()->getLongLivedSession();
         // Access Token
         $accessToken = $session->getToken();
